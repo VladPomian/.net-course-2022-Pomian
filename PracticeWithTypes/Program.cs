226b28b9c568833ae8543f017a74334fc9acd4ef
Polike897 <34polbof@mail.ru>
2022-08-30 13:13:22 +0300

    Ссылочные типы: присвоение сотруднику контракта и обновление стоимости валюты

---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Models;
+
+namespace PracticeWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Bill = new Employee { Name = "Bill" };
+            Bill.Contract = 3;
+            Console.WriteLine(Bill.Contract);
+
+            Employee Tom = new Employee { Name = "Tom", Age = 23, Contract = 4 };
+            Console.WriteLine(Tom.Contract);
+
+            Currency Dollars = new Currency { Price = 32 };
+            Console.WriteLine(Dollars.Price);
+        }
+    }
+}

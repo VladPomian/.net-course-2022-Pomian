5964856b45faaf09786bc812bc1ead5cb69afe23
Polike897 <34polbof@mail.ru>
2022-09-03 14:21:41 +0300

    Foundation

---
+++ Models/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.Models
+{
+    public class Account
+    {
+        public decimal Money { get; set; }
+        public Currency CurrencyType { get; set; }
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.Models
+{
+    public class Client : Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Passport { get; set; }
+    }
+}
